eb280e05c3baa573807051e3ddcbe50a2d8149a9
KaikCestari <kaikcestaridev@gmail.com>
2025-08-27 12:04:07 -0300

    inicio do pojeto

---
+++ src/main/java/devkaik7/EventApi/EventApiApplication.java
@@ -0,0 +1,10 @@
+package devkaik7.EventApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventApiApplication.class,
---
+++ src/test/java/devkaik7/EventApi/EventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package devkaik7.EventApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
